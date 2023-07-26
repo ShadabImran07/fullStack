65cc36032791a1a35cc535a4d86ed50862cc4c19
ShadabImran07 <shadabimran07@gmail.com>
2023-07-26 20:42:13 +0530

    for aws workiing

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import { Container, Card, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
+	const navigate = useNavigate();
 	return (
 		<div className=' py-5'>
 			<Container className='d-flex justify-content-center'>
@@ -18,7 +20,7 @@ const Hero = () => {
 					<div className='d-flex'>
 						<Button
 							variant='primary'
-							href='/login'
+							onClick={() => navigate("/login")}
 							className='me-3'
 						>
 							Sign In
